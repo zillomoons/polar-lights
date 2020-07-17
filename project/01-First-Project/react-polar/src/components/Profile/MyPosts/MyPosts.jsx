7e22e797b998f63d7509249427cfb94b50142c4e
zillomoons <dlzht73@ya.ru>
2020-07-17 17:21:01 +0800

    adding props to posts and likes

--- project/01-First-Project/react-polar/src/components/Profile/MyPosts/MyPosts.jsx
+++ project/01-First-Project/react-polar/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
-import Like from "./Post/Like";
 
 const MyPosts = () => {
+    let postsData = [
+        {id: 1, message: 'Hey, how are you?', likesCount: '5'},
+        {id: 2, message: 'Yo, everything is cool!', likesCount: '12'},
+        {id: 3, message: 'Stay calm and procced', likesCount: '27'}
+    ]
+
+    let postsElements = postsData.map(p => <Post message={p.message} likescount={p.likesCount}/>)
     return (
-            <div>My Posts
-                <div>
-                    <textarea></textarea>
-                    <button>Send</button>
-                </div>
-                <Post message='Hey, how are you?' />
-                <Like count='15' />
-                <Post message='Yo, everything is cool!' />
-                <Like count='20' />
+        <div className={s.myposts}>
+            <h3>My Posts</h3>
+            <div>
+                <textarea></textarea>
             </div>
+            <div>
+                <button>Send</button>
+            </div>
+            {postsElements}
+        </div>
 
     );
 }
