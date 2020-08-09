9351dde4c7ddf106d248f1be149bbf58fc6369f3
zillomoons <dlzht73@ya.ru>
2020-08-09 12:09:27 +0800

    creating class component Users

--- project/01-First-Project/react-polar/src/components/Users/Users.jsx
+++ project/01-First-Project/react-polar/src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         })
@@ -43,34 +42,3 @@ class Users extends React.Component {
 
 export default Users;
 
-/*[{id: 1,
-        photos: 'https://pm1.narvii.com/6532/db0c810124ee43a8d559782e5b8091b7f589cdc1_00.jpg',
-        followed: false,
-        name: 'Nicole',
-        status: 'doctor',
-        //location: {city: 'Elmore', country: 'USA'}
-    },
-    {
-        id: 2,
-        photos: 'https://pbs.twimg.com/profile_images/1226047118390910978/KpH5f8TI_400x400.jpg',
-        followed: true,
-        name: 'Gumball',
-        status: 'cool dude',
-        //location: {city: 'Elmore', country: 'USA'}
-    },
-    {
-        id: 3,
-        photos: 'https://i.pinimg.com/originals/c8/74/27/c8742733ec69bcf92870c7c5cfbbb1ef.jpg',
-        followed: true,
-        name: 'Marcy',
-        status: 'vampire',
-        //location: {city: 'somewhere', country: 'Woo'}
-    },
-    {
-        id: 4,
-        photos: 'https://cdn.getstickerpack.com/storage/uploads/sticker-pack/jake-the-dog-adventure-time/sticker_5.png?da79276a26129de11d0679eb2bfa6e6a',
-        followed: false,
-        name: 'Jake',
-        status: 'super-dog',
-        //location: {city: 'sweet kingdom', country: 'Woo'}
-    }]*/
