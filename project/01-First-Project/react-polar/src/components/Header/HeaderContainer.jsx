2f00adc0cf76032d3cb183a599b743227c398426
zillomoons <dlzht73@ya.ru>
2021-02-23 08:50:39 +0300

    created thunk for authReducer

--- project/01-First-Project/react-polar/src/components/Header/HeaderContainer.jsx
+++ project/01-First-Project/react-polar/src/components/Header/HeaderContainer.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import Header from './Header';
-import * as axios from 'axios';
 import { connect } from 'react-redux';
-import { setAuthUserData } from '../../redux/authReducer'
+import { setAuthData } from '../../redux/authReducer'
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/auth/me`, { withCredentials: true })
-      .then(response => {
-        if (response.data.resultCode === 0) {
-          let { id, email, login } = response.data.data;
-          this.props.setAuthUserData(id, email, login);
-        }
-      });
+    this.props.setAuthData();
   }
   render() {
     return <Header {...this.props} />
@@ -25,4 +17,4 @@ let mapStateToProps = (state) => ({
   login: state.auth.login
 })
 
-export default connect(mapStateToProps, { setAuthUserData })(HeaderContainer);
+export default connect(mapStateToProps, { setAuthData })(HeaderContainer);
