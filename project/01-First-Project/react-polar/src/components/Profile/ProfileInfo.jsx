207806e16ad2df16fbc9db50793987b9e9d509d0
zillomoons <dlzht73@ya.ru>
2021-02-25 09:10:31 +0300

    added local state to ProfileStatus

--- project/01-First-Project/react-polar/src/components/Profile/ProfileInfo.jsx
+++ project/01-First-Project/react-polar/src/components/Profile/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Preloader from '../Common/Preloader/preloader';
 import s from './ProfileInfo.module.css';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -8,11 +9,12 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div className={s.profile_image}>
+            {/* <div className={s.profile_image}>
                 <img src='https://m.dw.com/image/37030280_101.jpg' alt='polar lights' />
-            </div>
+            </div> */}
             <div className={s.description}>
                 <img src={props.profile.photos.large} />
+                <ProfileStatus status={'Hello, my friends!'} />
                 <p>Name: {props.profile.fullName}</p>
                 <p>About Me: {props.profile.aboutMe}</p>
                 <p>VK: {props.profile.contacts.vk}</p>
