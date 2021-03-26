4a0448da870ba7c36a25dc98dd5b1f7e3a0adc4e
zillomoons <dlzht73@ya.ru>
2021-03-26 18:40:31 +0300

    Updates

---
+++ precache-manifest.b389abe6571ea3dcef662b8f5ea9db1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d53ccb8802112bec993401378020014",
+    "url": "/polar-lights/index.html"
+  },
+  {
+    "revision": "66df3b3d5808675b07c2",
+    "url": "/polar-lights/static/css/3.7802525b.chunk.css"
+  },
+  {
+    "revision": "11c2dc0d21f52352fa89",
+    "url": "/polar-lights/static/css/4.e33d24a8.chunk.css"
+  },
+  {
+    "revision": "0fbb210a84fa3e76b432",
+    "url": "/polar-lights/static/css/main.79ee4475.chunk.css"
+  },
+  {
+    "revision": "eb25065e6e8a3b295117",
+    "url": "/polar-lights/static/js/2.d866c353.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/polar-lights/static/js/2.d866c353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66df3b3d5808675b07c2",
+    "url": "/polar-lights/static/js/3.2bb256aa.chunk.js"
+  },
+  {
+    "revision": "11c2dc0d21f52352fa89",
+    "url": "/polar-lights/static/js/4.5c8cec22.chunk.js"
+  },
+  {
+    "revision": "0fbb210a84fa3e76b432",
+    "url": "/polar-lights/static/js/main.9936cb22.chunk.js"
+  },
+  {
+    "revision": "ba6bcfb8529df5d13208",
+    "url": "/polar-lights/static/js/runtime-main.71627ea3.js"
+  },
+  {
+    "revision": "3fa434f6bcd98ca6271fc82e0c1623da",
+    "url": "/polar-lights/static/media/logo.3fa434f6.png"
+  },
+  {
+    "revision": "f59e750457cd47046cf7f97ebcdc70d8",
+    "url": "/polar-lights/static/media/preloader.f59e7504.svg"
+  }
+]);
